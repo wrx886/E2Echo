b882dff736becf2b2c02122785e06e902fa2a060
wrx886 <18750909886@139.com>
2025-02-07 15:51:49 +0800

    上传后端 API 文档，修改 WebSoctet 的枚举传参方案

--- server/src/main/java/com/github/wrx886/e2echo/server/web/wocket/handler/MessageHandler.java
+++ server/src/main/java/com/github/wrx886/e2echo/server/web/wocket/handler/MessageHandler.java
@@ -8,11 +8,8 @@ import org.springframework.web.socket.WebSocketSession;
 
 import com.github.wrx886.e2echo.server.common.LoginHold;
 import com.github.wrx886.e2echo.server.model.entity.Message;
-import com.github.wrx886.e2echo.server.model.enums.BaseEnum;
 import com.github.wrx886.e2echo.server.model.enums.MessageType;
 import com.github.wrx886.e2echo.server.model.vo.message.MessageVo;
-import com.github.wrx886.e2echo.server.result.E2EchoException;
-import com.github.wrx886.e2echo.server.result.ResultCodeEnum;
 import com.github.wrx886.e2echo.server.web.service.MessageService;
 
 @Controller
@@ -32,15 +29,7 @@ public class MessageHandler extends LoginHandler {
     }
 
     // 配置自动接收，如果配置为组消息，那么用户消息也会自动接收
-    public void registryAutoReceive(WebSocketSession session, Integer messageTypeCode) {
-        // 将 messageTypeCode 转为 messageType
-        MessageType messageType;
-        try {
-            messageType = convert(messageTypeCode, MessageType.class);
-        } catch (Exception e) {
-            throw new E2EchoException(ResultCodeEnum.WEB_SOCKET_REQUEST_PARAM_ERROR);
-        }
-
+    public void registryAutoReceive(WebSocketSession session, MessageType messageType) {
         // 注册自动接收
         messageService.registryAutoReceive(session.getId(), messageType);
     }
@@ -57,20 +46,6 @@ public class MessageHandler extends LoginHandler {
 
         // 取消配置自动接收
         messageService.cancelRegistryAutoReceive(session.getId());
-    }
-
-    // 将字符串的值转为枚举类型
-    private <T extends BaseEnum> T convert(Integer code, Class<T> targetType) {
-        // 通过 Class 获取枚举类型的所有实例
-        T[] ts = targetType.getEnumConstants();
-        // 遍历寻找
-        for (T t : ts) {
-            if (t.getCode().equals(code)) {
-                return t;
-            }
-        }
-        // 没有对应的类型
-        throw new IllegalArgumentException("code: " + code + " illegal");
     }
 
 }
