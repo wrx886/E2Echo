ea09e186ed6778970d6d16a74d876b932769a0a7
wrx886 <18750909886@139.com>
2026-02-10 12:09:20 +0800

    客户端：配置线程池

---
+++ e2echo-client/e2echo-client-common/src/main/java/com/github/wrx886/e2echo/client/common/config/ExecutorServiceConfig.java
@@ -0,0 +1,16 @@
+package com.github.wrx886.e2echo.client.common.config;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ExecutorServiceConfig {
+
+    @Bean
+    public ExecutorService executorService() {
+        return Executors.newCachedThreadPool();
+    }
+
+}
