5f2363572b8015f4c6512a564f264ae9d59e4653
wrx886 <18750909886@139.com>
2026-02-23 08:35:08 +0800

    网关：添加网关服务，满足分布式部署需求

---
+++ e2echo-server-gateway/src/main/java/com/github/wrx886/e2echo/server/gateway/Main.java
@@ -0,0 +1,15 @@
+package com.github.wrx886.e2echo.server.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
