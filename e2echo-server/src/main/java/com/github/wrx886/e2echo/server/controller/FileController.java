ad470e71b3af43d1964903c7bb901596fa227325
wrx886 <18750909886@139.com>
2026-02-23 10:49:13 +0800

    服务端：修复注解错误

--- e2echo-server/src/main/java/com/github/wrx886/e2echo/server/controller/FileController.java
+++ e2echo-server/src/main/java/com/github/wrx886/e2echo/server/controller/FileController.java
@@ -5,7 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -27,7 +26,7 @@ public class FileController {
 
     @Operation(summary = "上传文件")
     @PostMapping("/upload")
-    public Result<String> upload(@RequestPart @RequestParam MultipartFile file) {
+    public Result<String> upload(@RequestPart MultipartFile file) {
         return Result.ok(fileService.upload(file));
     }
 
